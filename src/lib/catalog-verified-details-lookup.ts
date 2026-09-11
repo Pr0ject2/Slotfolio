5d8a42e8b16d79304ea0471ecdb869ba2dfcc79a
Pr0ject2 <104135408+Pr0ject2@users.noreply.github.com>
2026-09-11 16:31:44 +0300

    Connect Play’n GO detail wave 15

--- src/lib/catalog-verified-details-lookup.ts
+++ src/lib/catalog-verified-details-lookup.ts
@@ -13,6 +13,7 @@ import { getCatalogVerifiedDetailsPlayngoWave11 } from "./catalog-verified-detai
 import { getCatalogVerifiedDetailsPlayngoWave12 } from "./catalog-verified-details-playngo-wave12";
 import { getCatalogVerifiedDetailsPlayngoWave13 } from "./catalog-verified-details-playngo-wave13";
 import { getCatalogVerifiedDetailsPlayngoWave14 } from "./catalog-verified-details-playngo-wave14";
+import { getCatalogVerifiedDetailsPlayngoWave15 } from "./catalog-verified-details-playngo-wave15";
 import { getCatalogVerifiedDetailsHacksaw } from "./catalog-verified-details-hacksaw";
 import { getCatalogVerifiedDetailsNolimit } from "./catalog-verified-details-nolimit";
 
@@ -33,6 +34,7 @@ export function getVerifiedCatalogDetails(slug: string) {
     getCatalogVerifiedDetailsPlayngoWave12(slug) ??
     getCatalogVerifiedDetailsPlayngoWave13(slug) ??
     getCatalogVerifiedDetailsPlayngoWave14(slug) ??
+    getCatalogVerifiedDetailsPlayngoWave15(slug) ??
     getCatalogVerifiedDetailsHacksaw(slug) ??
     getCatalogVerifiedDetailsNolimit(slug)
   );

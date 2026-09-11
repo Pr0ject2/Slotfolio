522bdded4e5bcdc689e541c7d4244aeac82700e7
Pr0ject2 <104135408+Pr0ject2@users.noreply.github.com>
2026-09-11 14:10:09 +0300

    Include third-wave Hacksaw research

--- src/lib/catalog-research-lookup.ts
+++ src/lib/catalog-research-lookup.ts
@@ -4,6 +4,7 @@ import { getCatalogResearch3OaksWave3 } from "./catalog-research-3oaks-wave3";
 import { getCatalogResearchEndorphina } from "./catalog-research-endorphina";
 import { getCatalogResearchHacksaw } from "./catalog-research-hacksaw";
 import { getCatalogResearchHacksawMore } from "./catalog-research-hacksaw-more";
+import { getCatalogResearchHacksawWave3 } from "./catalog-research-hacksaw-wave3";
 import { getCatalogResearchPlayngo } from "./catalog-research-playngo";
 import { getCatalogResearchPlayngoMore } from "./catalog-research-playngo-more";
 import { getCatalogResearchPlayngoThird } from "./catalog-research-playngo-third";
@@ -19,6 +20,7 @@ export function getVerifiedCatalogResearch(slug: string) {
     getCatalogResearchEndorphina(slug) ??
     getCatalogResearchHacksaw(slug) ??
     getCatalogResearchHacksawMore(slug) ??
+    getCatalogResearchHacksawWave3(slug) ??
     getCatalogResearchPlayngo(slug) ??
     getCatalogResearchPlayngoMore(slug) ??
     getCatalogResearchPlayngoThird(slug) ??
